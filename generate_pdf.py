1185b0fe3177ab5742dc755d40be5147099dca6e
KakkoiDev <contact@kakkoi.dev>
2026-03-18 15:59:10 +0900

    Remove English haiku translations from ja/mh PDFs
    Haiku had no Japanese translations, so t() fell back to English.
    Only show translations in the English PDF.

--- generate_pdf.py
+++ generate_pdf.py
@@ -430,7 +430,7 @@ def write_reading(w, categories, haiku, dialog_groups, dialogs, stories, lang):
             # Haiku
             for hk in by_sort(haiku_by_cat.get(cat['id'], [])):
                 mh_lines = to_ruby(hk['minihongo']).replace(' / ', ' \\\\ ')
-                if lang != 'mh':
+                if lang == 'en':
                     trans = to_ruby(t(hk, '', lang))
                     w.raw(f'#haiku-block[{mh_lines}][{trans}]')
                 else:
